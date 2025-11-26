78bb22e180be2754b8ff39c591483bf2aeaffa94
tylefnx <tayfunucuncu@gmail.com>
2025-11-26 17:26:44 +0300

    removed and replaced placeholder with the actual one

--- utils/checksum.js
+++ utils/checksum.js
@@ -1,12 +1,19 @@
-// basit CRC16 placeholder
-export function crc16(str) {
-  let hash = 0xFFFF;
-  for (let i = 0; i < str.length; i++) {
-    hash ^= str.charCodeAt(i);
-    for (let j = 0; j < 8; j++) {
-      if (hash & 1) hash = (hash >> 1) ^ 0xA001;
-      else hash = hash >> 1;
-    }
+export function internetChecksum(str) {
+  const buf = Buffer.from(str, "utf8");
+  let sum = 0;
+
+  // 16-bit bloklar halinde topla
+  for (let i = 0; i < buf.length; i += 2) {
+    // Son byte tek kalırsa padding yap
+    let word = (buf[i] << 8) + (buf[i + 1] || 0);
+    sum += word;
   }
-  return hash.toString(16).toUpperCase();
+
+  // Taşmaları (carry) ekle
+  while (sum > 0xFFFF) {
+    sum = (sum & 0xFFFF) + (sum >>> 16);
+  }
+
+  // 1'e tümleyenini al (Bitwise NOT)
+  return (~sum & 0xFFFF).toString(16).toUpperCase().padStart(4, "0");
 }
